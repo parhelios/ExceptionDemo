6848d9e42c9d30f6e702a747e200a788aeb04c95
niklas-hjelm <niklas.hjelm@iths.se>
2023-10-02 08:37:05 +0200

    Add project files.

---
+++ ClassLib/DemoException.cs
@@ -0,0 +1,6 @@
+﻿namespace ClassLib;
+
+public class DemoException : Exception
+{
+    public override string Message { get; } = "Detta är ett test!";
+}
---
+++ ExceptionDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassLib;
+using LINQDemo.Data;
+
+var db = new DataSource();
+
+var cart = db.GetCartForPerson(10);
+
+foreach (var product in cart)
+{
+    Console.WriteLine(product.Name);
+}
